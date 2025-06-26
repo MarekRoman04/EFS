dd6b41387a9701eb00595bfc43e587e8b8f362c0
Marek Roman <marekroman2004@gmail.com>
2025-06-26 09:36:24 +0200

    Test script basic string search tests

--- tests/test.bash
+++ tests/test.bash
@@ -1,59 +1,112 @@
 #!/bin/bash
 
-check_args()
+# --- CONFIG ---
+DEFAULT_TEST_PATH="./test_files"
+DEFAULT_PATTERNS_PATH="./patterns.txt"
+TEST_MODE=("binary" "random" "words")
+TEST_SIZE=("small" "medium" "large")
+
+run_tests()
 {
-    ARGS=$1
-    EXPECTED_ARGS=$2
-    if [ $ARGS != $EXPECTED_ARGS ]; then
-            echo "Expected $EXPECTED_ARGS argument" >&2
-            exit 1
-    fi
+    TEST_FUNCTION=$1
+    TEST_PATTERNS=${2:-$DEFAULT_PATTERNS_PATH}
+    TEST_FILES=${3:-$DEFAULT_TEST_PATH}
+
+    for MODE in "${TEST_MODE[@]}"; do
+            for FILE in "$TEST_FILES/$MODE"/*; do
+                echo "Checking: $FILE"
+                [ -f "$FILE" ] || continue
+                while IFS= read -r PATTERN; do
+                    "$TEST_FUNCTION" "$PATTERN" "$FILE"
+                done < "$TEST_PATTERNS"
+        done
+        echo "$MODE file test finished!"
+    done
 }
 
 string_search_test()
 {
-    check_args $# 2
-    echo "Checking $2"
-    EFS_RESULT=$(grep "$1" $2)
-    GREP_RESULT=$(grep "$1" $2)
+    # EFS_RESULT=...
+    GREP_RESULT="./temp/test_str_temp.txt"
+    grep -F "$1" "$2" > "$GREP_RESULT"
 
-    # RESULT_DIFF=$(diff $EFS_RESULT $GREP_RESULT)
+    if ! diff -q "$EFS_RESULT" "$GREP_RESULT" >/dev/null; then
+        echo "Error invalid line match in $FILE" >&2
+        diff "$EFS_RESULT" "$GREP_RESULT" >&2
+        exit 1
+    fi
+
+    # EFS_COUNT=...
+    GREP_COUNT=$(grep -Fc "$1" "$2")
+    # echo $GREP_COUNT
+
+    # if [ $EFS_COUNT != $GREP_COUNT ]; then
+    #     echo "Error invalid pattern count in $2" >&2
+    #     echo "Grep: $GREP_COUNT" >&2
+    #     echo "EFS: $EFS_COUNT" >&2
+    #     exit 1
+    # else
+    #     echo "$2: $GREP_COUNT"
+    # fi
+
+    # EFS_FULL_COUNT=...
+    GREP_FULL_COUNT=$(grep -Fo "$1" "$2" | wc -l)
+    # echo $GREP_FULL_COUNT
+
+    # if [ $EFS_FULL_COUNT != $GREP_FULL_COUNT ]; then
+    #     echo "Error invalid full pattern count in $2" >&2
+    #     echo "Grep | wc: $GREP_FULL_COUNT" >&2
+    #     echo "EFS: $EFS_FULL_COUNT" >&2
+    # fi
+
+    # EFS_WORD_RESULT=...
+    GREP_WORD_RESULT="./temp/test_w_str_temp.txt"
+    grep -Fw "$1" "$2" > "$GREP_WORD_RESULT"
+
+    # RESULT_DIFF=$(diff $EFS_WORD_RESULT $GREP_WORD_RESULT)
 
     # if [ $RESULT_DIFF ]; then
-    #     echo "Error invalid lines matched" >&2
+    #     echo "Error invalid lines matched in $2" >&2
     #     echo $RESULT_DIFF >&2
     #     exit 1
     # fi
 
-    EFS_COUNT=$(grep -c "$1" $2)
-    GREP_COUNT=$(grep -c "$1" $2)
+    # EFS_WORD_COUNT=...
+    GREP_WORD_COUNT=$(grep -Fcw "$1" "$2")
+    # echo $GREP_WORD_COUNT
 
-    if [ $EFS_COUNT != $GREP_COUNT ]; then
-        echo "Error invalid pattern count" >&2
-        echo "Grep: $GREP_COUNT" >&2
-        echo "TEST: $EFS_COUNT" >&2
-        exit 1
-    else
-        echo "$2: $GREP_COUNT"
-    fi
+    # if [ $EFS_WORD_COUNT != $GREP_WORD_COUNT ]; then
+    #     echo "Error invalid pattern count in $2" >&2
+    #     echo "Grep: $GREP_WORD_COUNT" >&2
+    #     echo "EFS: $EFS_WORD_COUNT" >&2
+    #     exit 1
+    # fi
+
+    # EFS_WORD_FULL_COUNT=...
+    GREP_WORD_FULL_COUNT=$(grep -Fow "$1" "$2" | wc -l)
+    # echo $GREP_WORD_FULL_COUNT
+
+    # if [ $EFS_WORD_FULL_COUNT != $GREP_WORD_FULL_COUNT ]; then
+    #     echo "Error invalid full pattern count in $2" >&2
+    #     echo "Grep | wc: $GREP_WORD_FULL_COUNT" >&2
+    #     echo "EFS: $EFS_WORD_FULL_COUNT" >&2
+    # fi
 }
 
-TEST_PATH="output_tests"
-TEST_MODE=("binary" "text_random" "text_words")
-TEST_SIZE=("small" "medium" "large")
+case "$1" in
+    all)
+        echo "Running all tests!"
+        ;;
+    string)
+        echo "Running string search tests!"
+        run_tests "string_search_test" $2 $3
+        ;;
+    regex)
+        echo "Running regex matching tests!"
+        ;;
+    *)
+        echo "Invalid arg given! Try --help for more information"
+        ;;
+esac
 
-
-echo "Running Tests..."
-
-for MODE in ${TEST_MODE[@]}; do
-    if [ $MODE != "binary" ]; then 
-        for SIZE in ${TEST_SIZE[@]}; do
-        FILE_PATH=$TEST_PATH/$MODE/$SIZE
-            for FILE in $FILE_PATH/*; do
-                string_search_test "unwilling" "$FILE"
-            done
-        done
-    fi
-    echo "$MODE passed!"
-done
 
