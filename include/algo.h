5a080fc5eed6751a0a898358b22a94ebcbf55adb
Marek Roman <marekroman2004@gmail.com>
2025-06-30 19:11:09 +0200

    BMH optimalizations, count search fix

--- include/algo.h
+++ include/algo.h
@@ -1,14 +1,15 @@
 #ifndef ALGO_H
 #define ALGO_H
 
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include "log.h"
 
 #define BMH_NOT_FOUND ((size_t)-1)
 
-int *bmh_pre_process(const char *pattern, const size_t length);
-size_t bmh_search(const int *table, const char *pattern, const size_t pattern_length, const char *string, const size_t string_length);
+unsigned char *bmh_pre_process(const char *pattern, const size_t length);
+size_t bmh_search(const unsigned char *table, const char *pattern, const size_t pattern_length, const char *string, const size_t string_length);
 int bmh_count(const char *pattern, const char *string);
 
 int string_search_count(const char *pattern, const char *f_path);
