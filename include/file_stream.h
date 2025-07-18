9e66dac69fda70f3a565e3b1b34c9e34d6a5a97d
Marek Roman <marekroman2004@gmail.com>
2025-07-18 10:33:57 +0200

    Line stream added, file stream fixes

--- include/file_stream.h
+++ include/file_stream.h
@@ -2,6 +2,11 @@
 #define FILE_STREAM_H
 
 #include <stdio.h>
+#include "log.h"
+
+//---------------------------------
+//----FILE STREAM DEFINITIONS------
+//---------------------------------
 
 typedef struct file_stream
 {
@@ -16,6 +21,15 @@ int fs_open_file(file_stream *fs, const char *mode);
 size_t fs_read_file(file_stream *fs, char *buffer, size_t buffer_size);
 int fs_close_file(file_stream *fs);
 int fs_end(file_stream *fs);
+
+static inline size_t fs_read(char *buffer, size_t buffer_size, FILE *fp, const char *file_path)
+{
+    size_t read = fread(buffer, 1, buffer_size, fp);
+    if (read < buffer_size && ferror(fp))
+        log_errno(0, file_path);
+
+    return read;
+}
 
 // Check if there is next file in stream
 static inline int fs_has_file(file_stream *fs)
@@ -39,6 +53,101 @@ static inline void fs_skip_file(file_stream *fs)
         fs->current_file++;
     else
         fs->current_file = NULL;
+}
+
+//---------------------------------
+//----LINE STREAM DEFINITIONS------
+//---------------------------------
+
+#define DEFAULT_LINE_BUFFER_SIZE 256
+
+typedef struct line_stream line_stream;
+typedef struct ls_internal ls_internal;
+typedef struct line_buffer line_buffer;
+typedef struct line_data line_data;
+
+struct line_stream
+{
+    const char *file_path;
+    line_data *line;
+    ls_internal *lsi;
+};
+
+// Contains internal data used in line stream
+struct ls_internal
+{
+    FILE *fp;
+    // File data buffer
+    char *buffer;
+    size_t buffer_idx;
+    size_t buffer_size;
+    size_t read;
+    // Empty buffer link list
+    line_buffer *empty_lb_head;
+    line_buffer *empty_lb_tail;
+};
+
+// Line buffer linked list node
+struct line_buffer
+{
+    char *buffer;
+    size_t buffer_idx;
+    size_t buffer_size;
+    line_buffer *next_buffer;
+};
+
+// Contains line data in line buffer linked list
+struct line_data
+{
+    const line_buffer *lb_head;
+    line_buffer *lb;
+    size_t line_length;
+};
+
+line_stream *fs_ls_init(const char *file_path, char *buffer, size_t buffer_size);
+int fs_ls_read_line(line_stream *ls);
+int fs_ls_end(line_stream *ls);
+
+// Resets line data buffer in line stream
+static inline void fs_ls_line_reset(line_stream *ls)
+{
+    ls->line->lb = (line_buffer *)ls->line->lb_head;
+    ls->line->lb->buffer_idx = 0;
+
+    // Moves unused buffers to ls_internal empty buffers
+    if (ls->line->lb->next_buffer)
+    {
+        if (ls->lsi->empty_lb_tail)
+            ls->lsi->empty_lb_tail->next_buffer = ls->line->lb->next_buffer;
+        else
+        {
+            ls->lsi->empty_lb_head = ls->line->lb->next_buffer;
+            ls->lsi->empty_lb_tail = ls->line->lb->next_buffer;
+        }
+
+        while (ls->lsi->empty_lb_tail->next_buffer)
+        {
+            ls->lsi->empty_lb_tail = ls->lsi->empty_lb_tail->next_buffer;
+            ls->lsi->empty_lb_tail->buffer_idx = 0;
+        }
+
+        ls->line->lb->next_buffer = NULL;
+    }
+}
+
+// Checks if there is next line in line stream
+static inline int fs_ls_has_line(line_stream *ls)
+{
+    if (ls->lsi->read)
+        return 0;
+
+    if ((ls->lsi->read = fs_read(ls->lsi->buffer, ls->lsi->buffer_size, ls->lsi->fp, ls->file_path)))
+    {
+        ls->lsi->buffer_idx = 0;
+        return 0;
+    }
+
+    return 1;
 }
 
 #endif
---
+++ src/file_stream/line_stream.c
@@ -0,0 +1,220 @@
+#include "file_stream.h"
+
+static inline line_buffer *line_buffer_alloc(size_t buffer_size);
+static inline void line_buffer_free(line_buffer *buffer);
+static inline ls_internal *ls_internal_init(const char *file_path, char *buffer, size_t buffer_size);
+line_stream *fs_ls_init(const char *file_path, char *buffer, size_t buffer_size);
+int fs_ls_read_line(line_stream *ls);
+int fs_ls_end(line_stream *ls);
+
+// Allocates line buffer node
+static inline line_buffer *line_buffer_alloc(size_t buffer_size)
+{
+    line_buffer *lb = (line_buffer *)malloc(sizeof(line_buffer));
+    if (!lb)
+    {
+        log_info("Error allocating memory line_buffer");
+        return NULL;
+    }
+
+    char *buffer = (char *)malloc(sizeof(char) * buffer_size);
+    if (!buffer)
+    {
+        log_info("Error allocating memory line_buffer buffer");
+        free(lb);
+        return NULL;
+    }
+
+    lb->buffer = buffer;
+    lb->buffer_idx = 0;
+    lb->buffer_size = buffer_size;
+    lb->next_buffer = NULL;
+    return lb;
+}
+
+// Frees all line buffers in linked list
+static inline void line_buffer_free(line_buffer *buffer)
+{
+    if (!buffer)
+        return;
+
+    line_buffer *lb = buffer;
+    line_buffer *lb_next = buffer->next_buffer;
+
+    while (lb_next)
+    {
+        free(lb);
+        lb = lb_next;
+        lb_next = lb->next_buffer;
+    }
+
+    free(lb);
+}
+
+static inline ls_internal *ls_internal_init(const char *file_path, char *buffer, size_t buffer_size)
+{
+    ls_internal *lsi = (ls_internal *)malloc(sizeof(ls_internal));
+    if (!lsi)
+    {
+        log_info("Error allocating memory line_stream_internal");
+        return NULL;
+    }
+
+    lsi->fp = fopen(file_path, "r");
+    if (!lsi->fp)
+    {
+        log_errno(0, file_path);
+        line_buffer_free(lsi->empty_lb_head);
+        free(lsi);
+        return NULL;
+    }
+
+    lsi->buffer = buffer;
+    lsi->buffer_idx = 0;
+    lsi->buffer_size = buffer_size;
+    lsi->read = 0;
+    lsi->empty_lb_head = NULL;
+    lsi->empty_lb_tail = NULL;
+}
+
+// Initializes line stream struct
+line_stream *fs_ls_init(const char *file_path, char *buffer, size_t buffer_size)
+{
+    line_stream *ls = (line_stream *)malloc(sizeof(line_stream));
+    if (!ls)
+    {
+        log_info("Error allocating memory line_stream");
+        return NULL;
+    }
+
+    ls->file_path = file_path;
+
+    ls->line = (line_data *)malloc(sizeof(line_data));
+    if (!ls->line)
+    {
+        log_info("Error allocating memory line_stream line_data");
+        free(ls);
+        return NULL;
+    }
+
+    line_buffer *lb = line_buffer_alloc(DEFAULT_LINE_BUFFER_SIZE);
+    if (!lb)
+    {
+        log_info("Error allocating memory line_stream line_data lb");
+        free(ls->line);
+        free(ls);
+        return NULL;
+    }
+
+    ls->line->lb_head = lb;
+    ls->line->lb = lb;
+
+    ls->lsi = ls_internal_init(file_path, buffer, buffer_size);
+    if (!ls->lsi)
+    {
+        log_info("Error allocating memory line_stream line_stream_internal");
+        line_buffer_free((line_buffer *)ls->line->lb_head);
+        free(ls->line);
+        free(ls);
+        return NULL;
+    }
+
+    return ls;
+}
+
+/*
+ * Reads data from file, overwrites previous line data buffers,
+ * lb point to firt buffer in list,
+ * unused buffers are returned to line stream.
+ */
+int fs_ls_read_line(line_stream *ls)
+{
+    fs_ls_line_reset(ls);
+    size_t line_length = 0;
+    int line_found = 0;
+
+    do
+    {
+        // Reads data from file
+        if (ls->lsi->read <= ls->lsi->buffer_idx)
+        {
+            ls->lsi->buffer_idx = 0;
+            ls->lsi->read = fs_read(ls->lsi->buffer, ls->lsi->buffer_size, ls->lsi->fp, ls->file_path);
+        }
+
+        if (!ls->lsi->read)
+        {
+            if (line_length)
+            {
+                line_found = 1;
+                break;
+            }
+            else
+            {
+                log_info("No data to read from!");
+                return 1;
+            }
+        }
+
+        // Reads line from buffer
+        while (ls->lsi->buffer_idx < ls->lsi->read)
+        {
+            char c = ls->lsi->buffer[ls->lsi->buffer_idx];
+            ls->line->lb->buffer[ls->line->lb->buffer_idx] = c;
+            ls->lsi->buffer_idx++;
+            ls->line->lb->buffer_idx++;
+            line_length++;
+
+            if (c == '\n')
+            {
+                line_found = 1;
+                break;
+            }
+
+            // Moves to next line buffer, allocates new buffer if all buffers are used
+            if (ls->line->lb->buffer_size <= ls->line->lb->buffer_idx)
+            {
+                if (ls->lsi->empty_lb_head)
+                {
+                    ls->line->lb->next_buffer = ls->lsi->empty_lb_head;
+                    ls->lsi->empty_lb_head = ls->lsi->empty_lb_head->next_buffer;
+                    ls->line->lb = ls->line->lb->next_buffer;
+                }
+                else
+                {
+                    ls->line->lb->next_buffer = line_buffer_alloc(DEFAULT_LINE_BUFFER_SIZE);
+                    if (!ls->line->lb->next_buffer)
+                    {
+                        log_info("Error allocating memory line_stream line line_buffer next_buffer");
+                        return 1;
+                    }
+
+                    ls->line->lb = ls->line->lb->next_buffer;
+                }
+            }
+        }
+
+    } while (!line_found);
+
+    ls->line->lb = (line_buffer *)ls->line->lb_head;
+    ls->line->line_length = line_length;
+    return 0;
+}
+
+// Ends line stream and frees all memory used
+int fs_ls_end(line_stream *ls)
+{
+    if (fclose(ls->lsi->fp))
+    {
+        log_info("Error closing file: %s", ls->file_path);
+        return 1;
+    }
+
+    line_buffer_free((line_buffer *)ls->line->lb_head);
+    free(ls->line);
+    line_buffer_free(ls->lsi->empty_lb_head);
+    free(ls->lsi);
+    free(ls);
+
+    return 0;
+}
