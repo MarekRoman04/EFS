21cae5604b337b391461a3567c9d86d6bb76ee6d
Marek Roman <marekroman2004@gmail.com>
2025-09-14 17:12:04 +0200

    File stream reworked

--- include/file_stream.h
+++ include/file_stream.h
@@ -1,6 +1,7 @@
 #ifndef FILE_STREAM_H
 #define FILE_STREAM_H
 
+#include <dirent.h>
 #include <stdio.h>
 #include "log.h"
 
@@ -8,59 +9,57 @@
 //----FILE STREAM DEFINITIONS------
 //---------------------------------
 
+#define NO_FILE_ERROR (size_t)-1
+
 typedef struct file_stream
 {
-    int file_count;
-    char **file_paths;
-    char **current_file;
-    FILE *current_fp;
+    const char *f_path;
+    FILE *fp;
 } file_stream;
 
-file_stream *fs_init(char **f_paths, int f_count);
-int fs_open_file(file_stream *fs, const char *mode);
-size_t fs_read_file(file_stream *fs, char *buffer, size_t buffer_size);
+/*
+ * Opens file stream from given file,
+ * file_stream->file_path is only shallow copy,
+ * file path is used only for debug logs,
+ * if opening failes file_stream->fp is NULL
+ */
+file_stream *fs_init(const char *f_path);
+/*
+ * Opens file in file stream, closes previous file,
+ * if closing failes file is not changed,
+ * if opening failes file_stream->fp is NULL,
+ */
+int fs_open_file(file_stream *fs, const char *f_path);
+/*
+ * Closes file in file stream, sets f_path to NULL,
+ * if close was successful
+ */
 int fs_close_file(file_stream *fs);
-int fs_end(file_stream *fs);
-
-static inline size_t fs_read(char *buffer, size_t buffer_size, FILE *fp, const char *file_path)
+/*
+ * Reads data from file stream into given buffer
+ */
+static inline size_t fs_read(file_stream *fs, char *buffer, size_t buffer_size)
 {
-    size_t read = fread(buffer, 1, buffer_size, fp);
-    if (read < buffer_size && ferror(fp))
-        log_errno(0, file_path);
+    if (!fs->fp)
+        return NO_FILE_ERROR;
+
+    size_t read = fread(buffer, 1, buffer_size, fs->fp);
+    if (read < buffer_size && ferror(fs->fp))
+        log_errno(0, fs->f_path);
 
     return read;
 }
-
-// Check if there is next file in stream
-static inline int fs_has_file(file_stream *fs)
-{
-    return fs->current_file + 1 < fs->file_paths + fs->file_count;
-}
-
-// Resest current file pointer to the beginning of the stream
-static inline void fs_reset(file_stream *fs)
-{
-    fs->current_file = fs->file_paths;
-}
-
-// Moves current file pointer to next file
-static inline void fs_skip_file(file_stream *fs)
-{
-    if (!fs->current_file)
-        return;
-
-    if (fs_has_file(fs))
-        fs->current_file++;
-    else
-        fs->current_file = NULL;
-}
+/*
+ * Frees memory used by file stream, closes opened file
+ */
+int fs_end(file_stream *fs);
 
 //---------------------------------
 //----LINE STREAM DEFINITIONS------
 //---------------------------------
 
 #define DEFAULT_LINE_BUFFER_SIZE 256
-#define LINE_BUFFER_EXP_GROW_LIMIT 64 * 1024 * 1024 // 64 MB
+#define LINE_BUFFER_EXP_GROW_LIMIT 64 * 1024 * 1024   // 64 MB
 #define LINE_BUFFER_LINEAR_GROW_SIZE 64 * 1024 * 1024 // 64 MB
 
 typedef struct line_stream line_stream;
@@ -88,5 +87,69 @@ line_stream *fs_ls_init(const char *file_path, char *buffer, size_t buffer_size)
 int fs_ls_read(line_stream *ls);
 int fs_ls_file(line_stream *ls, const char *file_path);
 int fs_ls_end(line_stream *ls);
+
+//---------------------------------
+//----DIR STREAM DEFINITIONS------
+//---------------------------------
+
+typedef struct dir_stream
+{
+    int dir_count;
+    char **dir_paths;
+    char **current_dir;
+    DIR *current_dp;
+} dir_stream;
+
+dir_stream *ds_init(char **dir_paths, int dir_count);
+int ds_open_dir(dir_stream *ds);
+int ds_close_dir(dir_stream *ds);
+int ds_end(dir_stream *ds);
+
+// Check if there is next directory in stream
+static inline int ds_has_dir(dir_stream *ds)
+{
+    return ds->current_dir + 1 < ds->dir_paths + ds->dir_count;
+}
+
+// Moves current directory pointer to the next directory in stream
+static inline void ds_skip_dir(dir_stream *ds)
+{
+    if (!ds->current_dir)
+        return;
+
+    if (ds_has_dir(ds))
+        ds->current_dir++;
+    else
+        ds->current_dir = NULL;
+}
+
+// Reads content from directory
+static inline struct dirent *ds_read(dir_stream *ds)
+{
+    if (!ds->current_dp)
+    {
+        log_info("No directory to read from!");
+        return NULL;
+    }
+
+    errno = 0;
+    struct dirent *dp = readdir(ds->current_dp);
+
+    if (dp == NULL)
+    {
+        if (errno == 0)
+        {
+            log_info("No file remaining in directory");
+            return NULL;
+        }
+        else
+        {
+            log_errno(0, *(ds->current_dir));
+            return NULL;
+        }
+    }
+
+    return dp;
+}
 
 #endif
--- src/file_stream/file_stream.c
+++ src/file_stream/file_stream.c
@@ -1,14 +1,28 @@
 #include "file_stream.h"
 
-file_stream *fs_init(char **f_paths, int f_count);
-int fs_open_file(file_stream *fs, const char *mode);
-size_t fs_read_file(file_stream *fs, char *buffer, size_t buffer_size);
-void fs_skip_file(file_stream *fs);
+file_stream *fs_init(const char *f_path);
+int fs_open_file(file_stream *fs, const char *f_path);
 int fs_close_file(file_stream *fs);
 int fs_end(file_stream *fs);
 
-// Initializes file stream struct
-file_stream *fs_init(char **f_paths, int f_count)
+static inline int fs_open(file_stream *fs, const char *f_path)
+{
+    errno = 0;
+    fs->f_path = f_path;
+    fs->fp = fopen(f_path, "r");
+    if (!fs->fp)
+    {
+        log_info("Error opening file: %s", f_path);
+        if (errno)
+            log_errno(0, f_path);
+
+        return errno ? errno : 1;
+    }
+
+    return 0;
+}
+
+file_stream *fs_init(const char *f_path)
 {
     file_stream *fs = (file_stream *)malloc(sizeof(file_stream));
     if (!fs)
@@ -16,81 +30,52 @@ file_stream *fs_init(char **f_paths, int f_count)
         log_info("Error allocating memory!");
         return NULL;
     }
+    fs->f_path = NULL;
+    fs->fp = NULL;
 
-    fs->file_paths = f_paths;
-    fs->file_count = f_count;
-    fs->current_file = f_paths;
-    fs->current_fp = NULL;
+    if (f_path)
+        fs_open(fs, f_path);
 
     return fs;
 }
 
-// Opens file in file stream, moves to next file if file is already opened
-int fs_open_file(file_stream *fs, const char *mode)
+int fs_open_file(file_stream *fs, const char *f_path)
 {
-    if (fs->current_fp && fs_close_file(fs))
+    if (!fs->fp)
     {
-        log_info("Can not move to next file!");
-        return 1;
+        return fs_open(fs, f_path);
     }
 
-    if (!fs->current_file)
-    {
-        log_info("No file remaining in stream!");
-        return 1;
-    }
+    if (fs_close_file(fs))
+        return errno ? errno : 1;
 
-    fs->current_fp = fopen(*(fs->current_file), mode);
-    if (!fs->current_fp)
-    {
-        log_errno(0, *(fs->current_file));
-        return 1;
-    }
-
-    return 0;
+    return fs_open(fs, f_path);
 }
 
-// Reads data from file to buffer from file stream, returns number of bytes read
-size_t fs_read_file(file_stream *fs, char *buffer, size_t buffer_size)
-{
-    if (!fs->current_fp)
-    {
-        log_info("No file opened to read from!");
-        return 0;
-    }
-
-    return fs_read(buffer, buffer_size, fs->current_fp, *(fs->current_file));
-}
-
-// Closes currently opened file in file stream and moves current file pointer to next file
 int fs_close_file(file_stream *fs)
 {
-    if (!fs->current_fp)
+    if (!fs->fp)
+        return 0;
+
+    errno = 0;
+    if (fclose(fs->fp))
     {
-        log_info("No file to close!");
-        return 1;
+        log_info("Error closing file: %s", fs->f_path);
+        if (errno)
+            log_errno(0, fs->f_path);
+
+        return errno ? errno : 1;
     }
 
-    if (fclose(fs->current_fp))
-    {
-        log_info("Error closing file: %s", *(fs->current_file));
-        return 1;
-    }
-
-    fs->current_fp = NULL;
-    fs_skip_file(fs);
-
+    fs->f_path = NULL;
+    fs->fp = NULL;
     return 0;
 }
 
-// Ends given filestream, frees memory used by file stream struct
 int fs_end(file_stream *fs)
 {
-    if (fs->current_fp && fs_close_file(fs))
-    {
-        log_info("Error ending file stream!");
+    if (fs_close_file(fs))
         return 1;
-    }
 
     free(fs);
     return 0;
